7b3342e35c8aadd131f8ceff721438de6dca7941
kaduelinho <carloseduardolibardo@gmail.com>
2018-04-09 05:14:12 -0300

    Revert " First prototype of iteration"
    This reverts commit f5c8d8b5db1b349a3d9ea74d7c1775b510f4e05e.

--- project/js/button_interaction.js
+++ project/js/button_interaction.js
@@ -1,45 +1,29 @@
 
 function createButtonInteraction(){
-    var foundSubmit = document.getElementById("btn_submit");
-    var foundInput = document.getElementById("url_input");
-    var foundReset = document.getElementById("btn_reset");
-    foundSubmit.onclick  = change_button;
-    foundReset.onclick = reset_page;
-    //Back to initial setup
-    function reset_page(){
-        foundSubmit.value = "ENCURTAR";
-        foundReset.classList.remove("pressReset");
-        foundSubmit.classList.remove("acendeSubmit");
-        foundSubmit.classList.remove("apagaSubmit");
-    }    
-
+    var teste = document.getElementById("btn_submit");
+    teste.onclick  = change_button;
+    
 
     function change_button(){
-        // Check value of submit button and if results is true, does the animation and swap url for shortened url 
-        if (foundSubmit.value == "ENCURTAR"){
-            if (foundInput.value != ""){
-                foundSubmit.classList.add("apagaSubmit");
-                setTimeout(function() {
-                    foundSubmit.classList.add("acendeSubmit");
-                    foundSubmit.value = "COPIAR";
-                    foundReset.classList.add("pressReset");
-                    foundInput.value = makeid();
-                }, 1000);          
-            }
-        }
-        // If results is false, copy the shortened url
-        else{
-            foundInput.select();
-            document.execCommand("Copy"); 
-        }  
+
+        var teste2 = document.getElementById("url_input");
+            teste2.value = "ALA"
+            teste.value = "COPIAR";
+            console.log(makeid());
+            setTimeout(function() {
+                
+            }, 1000);
+            
     }
-    // Shortened url generator
     function makeid() {
         var text = "";
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (var i = 0; i < 5; i++)
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
       
-        return "http://chr.dc/" + text;
-      }      
+        for (var i = 0; i < 5; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+      
+        return text;
+      }
+      
+      
 }
