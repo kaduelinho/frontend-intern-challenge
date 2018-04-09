62c20359e1ad5475c35085b910069bd332d40846
Carlos Eduardo Libardo <carloseduardolibardo@gmail.com>
2018-04-08 22:30:35 -0300

    Add short url generator

--- project/js/button_interaction.js
+++ project/js/button_interaction.js
@@ -1,8 +1,29 @@
+
 function createButtonInteraction(){
     var teste = document.getElementById("btn_submit");
     teste.onclick  = change_button;
+    
 
     function change_button(){
-        teste.value= "COPIAR";
+
+        var teste2 = document.getElementById("url_input");
+            teste2.value = "ALA"
+            teste.value = "COPIAR";
+            console.log(makeid());
+            setTimeout(function() {
+                
+            }, 1000);
+            
     }
+    function makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      
+        for (var i = 0; i < 5; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+      
+        return text;
+      }
+      
+      
 }
