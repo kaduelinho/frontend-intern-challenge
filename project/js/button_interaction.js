0422e4b10a02f2e19854031bf753aa9af6cecedb
kaduelinho <carloseduardolibardo@gmail.com>
2018-04-09 12:48:32 -0300

    corrected and improved the iteration effect

--- project/js/button_interaction.js
+++ project/js/button_interaction.js
@@ -11,20 +11,23 @@ function createButtonInteraction(){
         foundReset.classList.remove("pressReset");
         foundSubmit.classList.remove("acendeSubmit");
         foundSubmit.classList.remove("apagaSubmit");
+        foundInput.classList.remove("apagaInput");
+        foundInput.classList.remove("acendeInput");
     }    
-
 
     function change_button(){
         // Check value of submit button and if results is true, does the animation and swap url for shortened url 
         if (foundSubmit.value == "ENCURTAR"){
             if (foundInput.value != ""){
                 foundSubmit.classList.add("apagaSubmit");
+                foundInput.classList.add("apagaInput");
                 setTimeout(function() {
                     foundSubmit.classList.add("acendeSubmit");
                     foundSubmit.value = "COPIAR";
                     foundReset.classList.add("pressReset");
+                    foundInput.classList.add("acendeInput");
                     foundInput.value = makeid();
-                }, 1000);          
+                }, 700);          
             }
         }
         // If results is false, copy the shortened url
