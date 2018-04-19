6a1bca6255cf370f57b1136a5368ec6700c0ee5b
kaduelinho <carloseduardolibardo@gmail.com>
2018-04-18 22:50:02 -0300

    Added scripts in html

--- project/js/makeId.js
+++ project/js/makeId.js
@@ -1,13 +1,4 @@
 // Shortened url generator
-function makeid() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-  
-    return "http://chr.dc/" + text;
-}
-
 function hashCode(str){
 var hash = 0;
 if (str.length == 0) return hash;
